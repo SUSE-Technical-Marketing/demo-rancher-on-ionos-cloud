03704751ed0de78ba2b0935580a3314074c46952
Dominik Wombacher <dominik.wombacher@suse.com>
2025-12-11 22:33:03 +0100

    feat(tf): cloud-init move rke2 install logic into one-shot systemd service

--- cloud_init.tf
+++ cloud_init.tf
@@ -7,13 +7,6 @@ data "cloudinit_config" "server_rancher_first" {
   base64_encode = true
 
   part {
-    filename     = "rke2.sh"
-    content_type = "text/x-shellscript"
-
-    content = module.rke2_first.rke2_user_data
-  }
-
-  part {
     filename     = "cloud-config.yaml"
     content_type = "text/cloud-config"
 
@@ -23,27 +16,51 @@ data "cloudinit_config" "server_rancher_first" {
       create_hostname_file: true
       prefer_fqdn_over_hostname: false
 
+      write_files:
+        - path: /opt/rke2-install.sh
+          permissions: '0755'
+          encoding: b64
+          content: ${base64encode(module.rke2_first.rke2_user_data)}
+
+        - path: /etc/systemd/system/rke2-installer.service
+          permissions: '0644'
+          content: |
+            [Unit]
+            Description=Install RKE2 (10 Retries)
+            After=network-online.target
+            Wants=network-online.target
+
+            # Allow 10 starts within a 10-minute window.
+            # If we hit 10 failures, stop trying.
+            StartLimitBurst=10
+            StartLimitIntervalSec=600
+
+            [Service]
+            Type=oneshot
+            ExecStart=/opt/rke2-install.sh
+
+            # If script fails, wait 30 seconds before restart
+            Restart=on-failure
+            RestartSec=30s
+
+            [Install]
+            WantedBy=multi-user.target
+
       runcmd:
       - SUSEConnect -r ${var.scc_registration_code} -e ${var.scc_registration_email}
       - zypper ref && zypper --non-interactive in iptables
       - zypper --non-interactive dup
+      - systemctl daemon-reload
+      - systemctl enable --now rke2-installer.service
     EOT
   }
 }
-
 
 data "cloudinit_config" "server_rancher_additional" {
   count = 2
 
   gzip          = false
   base64_encode = true
-
-  part {
-    filename     = "rke2.sh"
-    content_type = "text/x-shellscript"
-
-    content = module.rke2_additional.rke2_user_data
-  }
 
   part {
     filename     = "cloud-config.yaml"
@@ -55,10 +72,42 @@ data "cloudinit_config" "server_rancher_additional" {
       create_hostname_file: true
       prefer_fqdn_over_hostname: false
 
+      write_files:
+        - path: /opt/rke2-install.sh
+          permissions: '0755'
+          encoding: b64
+          content: ${base64encode(module.rke2_first.rke2_user_data)}
+
+        - path: /etc/systemd/system/rke2-installer.service
+          permissions: '0644'
+          content: |
+            [Unit]
+            Description=Install RKE2 (10 Retries)
+            After=network-online.target
+            Wants=network-online.target
+
+            # Allow 10 starts within a 10-minute window.
+            # If we hit 10 failures, stop trying.
+            StartLimitBurst=10
+            StartLimitIntervalSec=600
+
+            [Service]
+            Type=oneshot
+            ExecStart=/opt/rke2-install.sh
+
+            # If script fails, wait 30 seconds before restart
+            Restart=on-failure
+            RestartSec=30s
+
+            [Install]
+            WantedBy=multi-user.target
+
       runcmd:
       - SUSEConnect -r ${var.scc_registration_code} -e ${var.scc_registration_email}
       - zypper ref && zypper --non-interactive in iptables
       - zypper --non-interactive dup
+      - systemctl daemon-reload
+      - systemctl enable --now rke2-installer.service
       - reboot
     EOT
   }
